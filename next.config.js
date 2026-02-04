e5b7eab7b6b524f7cb7746c3ca3ab83c838d5225
Adnan 2025 <adaptiveworkflowconsultancy@gmail.com>
2026-02-04 10:23:23 +0000

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  images: { unoptimized: true },
+  output: 'standalone',
+  trailingSlash: false,
+  images: {
+    unoptimized: true,
+  },
 };
+
 module.exports = nextConfig;
